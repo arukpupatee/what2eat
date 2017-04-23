21c438ee3d07596b47fb441986fed2a1ad8d9053
Aruk Pupatee <aruk.pupatee@gmail.com>
2017-04-24 01:06:20 +0700

    Add basic page

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+app.get('/find', function(req, res) {
+    res.render('pages/find');
+});
+
+app.listen(8080);
+console.log('Server is running');
